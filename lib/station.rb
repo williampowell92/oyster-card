73a94e21a381c781baf7707a52899c15da0a8524
William Powell <william.powell92@gmail.com>
2018-04-23 17:55:24 +0100

    Station looks up zone on initialize

--- lib/station.rb
+++ lib/station.rb
@@ -1,7 +1,19 @@
+require 'csv'
+
 class Station
+  STATIONS_FILEPATH = './resources/stations.csv'
+
   attr_reader :name, :zone
-  def initialize(name, zone)
+  def initialize(name)
     @name = name
-    @zone = zone
+    @zone = find_zone(name)
+  end
+
+  def find_zone(name)
+    CSV.foreach(STATIONS_FILEPATH) do |line|
+      station, zone = line
+      return zone.to_i if station == name
+    end
+    raise 'Station not found'
   end
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,16 +1,15 @@
-
 describe Station, :station do
   describe '#initialize' do
     let(:name) { 'Aldgate East' }
     let(:zone) { 1 }
-    let(:station) { Station.new(name, zone)}
+    let(:station) { Station.new(name)}
 
     it 'initializes with a name' do
       expect(station.name).to eq name
     end
 
     it 'initializes with a zone' do
-      expect(station.zone).to eq zone
+      expect(station.zone).to eq 1
     end
   end
 end
