59256241a57be4ecfe1f6fe3777c7227bd2b0991
Muzzi Aldean <amiraldean@gmail.com>
2018-04-25 14:11:27 +0100

    not touching out feature test

--- spec/features/oyster_card_feature_spec.rb
+++ spec/features/oyster_card_feature_spec.rb
@@ -1,4 +1,18 @@
 require 'spec_helper'
 
-feature OysterCard do
+feature 'London Underground' do
+  let(:station_1) {Station.new('Aldgate East')}
+  let(:station_2) {Station.new('Euston')}
+  let(:oyster_card) {Oystercard.new}
+  PENALTY_FARE = 6000
+
+  scenario 'not touching out' do
+    oyster_card.top_up(PENALTY_FARE)
+    oyster_card.touch_in(station_1)
+    oyster_card.touch_in(station_2)
+    expect(oyster_card.balance).to eq(0)
+    expect(oyster_card.current_journey).to eq({entry_station: station_2})
+    expect(oyster_card.journeys).to include({entry_station: station_1})
+  end 
+
 end
