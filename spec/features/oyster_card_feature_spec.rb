e2b9ffc86f40c544bfb0650356bb38b44fe929b9
William Powell <william.powell92@gmail.com>
2018-04-25 14:21:23 +0100

    Created commented out feature test

--- spec/features/oyster_card_feature_spec.rb
+++ spec/features/oyster_card_feature_spec.rb
@@ -1,18 +1,30 @@
 require 'spec_helper'
 
 feature 'London Underground' do
+=begin
   let(:station_1) {Station.new('Aldgate East')}
   let(:station_2) {Station.new('Euston')}
-  let(:oyster_card) {Oystercard.new}
+  let(:oyster_card) {OysterCard.new}
   PENALTY_FARE = 6000
 
-  scenario 'not touching out' do
-    oyster_card.top_up(PENALTY_FARE)
-    oyster_card.touch_in(station_1)
-    oyster_card.touch_in(station_2)
-    expect(oyster_card.balance).to eq(0)
-    expect(oyster_card.current_journey).to eq({entry_station: station_2})
-    expect(oyster_card.journeys).to include({entry_station: station_1})
-  end 
+  context 'touching in twice' do
+    before do
+      oyster_card.top_up(PENALTY_FARE)
+      oyster_card.touch_in(station_1)
+      oyster_card.touch_in(station_2)
+    end
 
+    scenario 'takes a penalty fare' do
+      expect(oyster_card.balance).to eq(0)
+    end
+
+    scenario 'stores latest touch in to entry station' do
+      expect(oyster_card.current_journey).to eq({entry_station: station_2})
+    end
+
+    scenario 'stores the last journey without an exit station' do
+      expect(oyster_card.journeys).to include({entry_station: station_1})
+    end
+  end
+=end
 end
