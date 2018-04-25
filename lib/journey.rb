299df028e555ca228af1f1ce220e4c8ff565c569
William Powell <william.powell92@gmail.com>
2018-04-25 14:41:48 +0100

    Added start journey method to Journey class

--- lib/journey.rb
+++ lib/journey.rb
@@ -2,6 +2,14 @@ class Journey
   attr_reader :entry_station, :exit_station
 
   def initialize
+    @in_journey = false
+  end
 
+  def in_journey?
+    @in_journey
+  end
+
+  def start_journey
+    @in_journey = true
   end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -5,7 +5,18 @@ describe Journey do
     end
 
     it 'creates an empty exit station' do
-      expect(subject.exit_station).to be_nil 
+      expect(subject.exit_station).to be_nil
+    end
+
+    it 'initializes out of journey' do
+      expect(subject).not_to be_in_journey
+    end
+  end
+
+  describe '#start_journey' do
+    it 'changes in journey to true' do
+      subject.start_journey
+      expect(subject).to be_in_journey
     end
   end
 end
